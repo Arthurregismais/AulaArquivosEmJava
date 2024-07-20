04cc928187e7cf4f69ae0763575cd7e7263d7c9e
Arthurregismais <Arthur@DESKTOP-PRVT7GH>
2024-07-20 18:27:51 -0300

    Editando Célula existente no XLS

---
+++ arquivosEmJava/src/arquivos/ApachPoiEditando2.java
@@ -0,0 +1,49 @@
+package arquivos;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ApachPoiEditando2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		File file = new File("C:\\Users\\Arthur\\git\\arquivosEmJava\\arquivosEmJava\\src\\arquivos\\arquivo_excel.xls");
+		
+		FileInputStream entrada = new FileInputStream(file);
+		
+		HSSFWorkbook hssfWorkBook = new HSSFWorkbook(entrada);
+		HSSFSheet planilha = hssfWorkBook.getSheetAt(0);
+		
+		Iterator<Row> linhaIterator = planilha.iterator();
+		
+		
+		while (linhaIterator.hasNext()) {
+			Row linha = linhaIterator.next();
+			
+			int numeroCelulas = linha.getPhysicalNumberOfCells();
+			
+			String valorCelula = linha.getCell(0).getStringCellValue();
+			
+			linha.getCell(0).setCellValue(valorCelula + " * valor gravado na aula *");
+		}
+		
+		entrada.close();
+		
+		FileOutputStream saida = new FileOutputStream(file);
+		hssfWorkBook.write(saida);
+		saida.flush();
+		saida.close();
+		
+		System.out.println("Planilha atualizada ");
+		
+	}
+	
+}
